4eaaa46c702162973dcf4d3ad7b22a48032cd1ee
Nick Diemer <nick.diemer@revature.com>
2021-05-06 10:02:26 -0400

    reimplemented PizzaBox.Domain and added interfaces, and repositories

---
+++ PizzaBox.Domain/Models/Order.cs
@@ -0,0 +1,19 @@
+using System.Linq;
+// using PizzaBox.Domain.Abstracts;
+
+namespace PizzaBox.Domain.Models
+{
+  public class Order
+  {
+    // public Customer Customer { get; set; }
+    // public AStore Store { get; set; }
+    // public APizza Pizza { get; set; }
+    // public decimal TotalCost
+    // {
+    //   get
+    //   {
+    //     return Pizza.Crust.Price + Pizza.Size.Price + Pizza.Toppings.Sum(t => t.Price);
+    //   }
+    // }
+  }
+}
